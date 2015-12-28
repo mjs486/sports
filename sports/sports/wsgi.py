87d1ade95a57a076541769dedc855a3041863e8a
Maxwell Sanders <mjs486@cornell.edu>
2015-12-28 04:37:41 -0500

    trying wsgihandler()

--- sports/sports/wsgi.py
+++ sports/sports/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sports.settings")
-
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
