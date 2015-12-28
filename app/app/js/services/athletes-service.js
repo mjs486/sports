408e2fdcea603262e90db6c6b1912105242800d9
Maxwell Sanders <mjs486@cornell.edu>
2015-12-28 15:20:26 -0500

    im an idiot

--- app/app/js/services/athletes-service.js
+++ app/app/js/services/athletes-service.js
@@ -1,12 +1,9 @@
 angular.module('sportsServices')
 
 .factory('Athletes', ['$resource',
-    function($resource){
-	    return $resource('/sports/api/athlete/:id/', {}, {
-	      query: {method:'GET', params:{id:'@id'}, isArray:true},
-	      post: {method:'POST'},
-	      update: {method:'PUT', params: {id: '@id'}},
-	      remove: {method:'DELETE', params: {id: '@id'}},
-	      stripTrailingSlashes: false
-	});
-}])
+  function($resource){
+    return $resource('/sports/api/athlete/?sport=:sportId&headline=:headline', {}, {
+      query: {method:'GET', params:{sportId:'@sportId',headline : '@headline'}, isArray:true},
+      stripTrailingSlashes: false
+    });
+  }])
